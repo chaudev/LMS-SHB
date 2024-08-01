afae8c628d24e4119c32eb966e7ab5f60a95c580
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2024-08-01 15:00:56 +0700

    hiển thị file pdf, excel... ra bảng tin

--- src/common/components/News/files.tsx
+++ src/common/components/News/files.tsx
@@ -1,6 +1,7 @@
 import { Image, Modal } from 'antd'
+import Link from 'next/link'
 import React, { FC, useEffect, useState } from 'react'
-import { FaPlay } from 'react-icons/fa'
+import { FaFile, FaPlay } from 'react-icons/fa'
 
 type TNewsFiles = {
 	files: Array<{ FileType: string; FileUrl: string; FileName: string }>
@@ -51,36 +52,54 @@ const NewsFiles: FC<TNewsFiles> = React.memo(({ files }) => {
 			{files.length > 0 && <div className="cc-hr my-[8px] mx-[-6px]" />}
 
 			<div className={`grid grid-cols-${imagesLength} gap-x-2 gap-y-2`}>
-				{images.map((item, index) => {
-					return (
-						<div
-							key={`FILE-POST-${index}-${new Date().getTime()}`}
-							onClick={() => setVisible(item.src)}
-							className={`shadow-md cursor-pointer bg-[#fff] h-full`}
-						>
-							{item.fileType !== 'mp4' && (
-								<img
-									draggable={false}
-									src={item.src}
-									className={`${imagesLength > 1 ? 'max-h-[400px] w-[100%]' : ''} object-cover`}
-									width="100%"
-								/>
-							)}
+				{images?.length > 0
+					? images.map((item, index) => {
+							return (
+								<div
+									key={`FILE-POST-${index}-${new Date().getTime()}`}
+									onClick={() => setVisible(item.src)}
+									className={`shadow-md cursor-pointer bg-[#fff] h-full`}
+								>
+									{item.fileType !== 'mp4' && (
+										<img
+											draggable={false}
+											src={item.src}
+											className={`${imagesLength > 1 ? 'max-h-[400px] w-[100%]' : ''} object-cover`}
+											width="100%"
+										/>
+									)}
 
-							{(item.fileType == 'mp4' || item.fileType == 'avi' || item.fileType == 'mkv') && (
-								<div className="relative text-[22px] hover:text-[24px]">
-									<video className={`${imagesLength > 1 ? 'h-[120px] w-[100%]' : 'min-h-[400px]'} object-cover`} id="video" src={item.src}>
-										<source src={item.src} className="min-h-[400px]" type="video/mp4" />
-										Your browser does not support the video tag.
-									</video>
-									<div className="top-0 lef-0 right-0 absolute bottom-0 flex all-center bg-[#00000041] h-full w-full">
-										<FaPlay className="" color="#fff" />
-									</div>
+									{(item.fileType == 'mp4' || item.fileType == 'avi' || item.fileType == 'mkv') && (
+										<div className="relative text-[22px] hover:text-[24px]">
+											<video
+												className={`${imagesLength > 1 ? 'h-[120px] w-[100%]' : 'min-h-[400px]'} object-cover`}
+												id="video"
+												src={item.src}
+											>
+												<source src={item.src} className="min-h-[400px]" type="video/mp4" />
+												Your browser does not support the video tag.
+											</video>
+											<div className="top-0 lef-0 right-0 absolute bottom-0 flex all-center bg-[#00000041] h-full w-full">
+												<FaPlay className="" color="#fff" />
+											</div>
+										</div>
+									)}
 								</div>
-							)}
-						</div>
-					)
-				})}
+							)
+					  })
+					: files?.map((item) => (
+							<div key={crypto.randomUUID()}>
+								{
+									<Link href={item?.FileUrl}>
+										<a className="!text-primary hover:underline">
+											<div className="">
+												<FaFile /> {item?.FileName}
+											</div>
+										</a>
+									</Link>
+								}
+							</div>
+					  ))}
 			</div>
 
 			<Modal width={1000} closable={false} centered open={!!visible} footer={null} onCancel={() => setVisible('')}>
